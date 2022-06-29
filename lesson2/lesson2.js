9346b1e685a6be2ab3679f0730de54dd2d8b0bc6
podporin96 <83721879+podporin96@users.noreply.github.com>
2022-06-29 15:44:02 +0700

    homework_2

---
+++ lesson2/lesson2.js
@@ -0,0 +1,55 @@
+//Задавние Вывести четные ч исла от 10 до 50
+
+// for (let i=10; i<=50; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i)
+//     }
+// }
+
+// const profile ={
+//     name: 'Vladimer',
+//     lastName: 'Podporin',
+//     age: 25,
+//     pets: false
+// }
+
+const array = [
+    'я в Симбирск,',
+    'в трактире.',
+    'с утра',
+    'В ту же ночь',
+    'Я остановился',
+    'для закупки', 
+    'что и было поручено Савельичу.',
+    'приехал,',
+    'где должен был',
+    'нужных вещей',
+    'отправился по лавкам',
+    'пробыть сутки',
+    'Савельич'
+]
+// console.log(array[3],array[7],array[0],array[8],array[11],array[5],array[9],array[6],array[4],array[1],array[12],array[2],array[10])
+// result =array[3]+array[7]+' '+array[0]+' '+array[8]+' '+array[11]+' '+array[5]+' '+array[9]+' '+array[6]+array[4]+array[1]+array[12]+' '+array[2]+' '+array[10] 
+// console.log(result)//из массова собирается строка
+// a=typeof(result)//проверял тип
+// console.log(a)
+
+// const person = (firstName,lastName)=> {
+//     const fullName = `${firstName} ${lastName}`;
+//     console.log(fullName);
+// }
+
+// person('Vladimer','Podporin')
+
+
+//Задание:Вывести в консоль нечетные числа
+let i =21;
+while (i<=67) {
+    if (i%2!=0) {
+        console.log(i)        
+    } 
+    // else {
+    //     console.log('Четное число')
+    // }
+    i++;
+    } 
